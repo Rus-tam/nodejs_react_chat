6d4de9b211c07e5dfa41b40f6cb97b4e868b0fb4
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-06-06 23:52:43 +0500

    sending username

---
+++ README.md
@@ -0,0 +1,4 @@
+<h3>Чат на NodeJS и ReactJS</h3>
+
+Учебный проект. Чат построен на NodeJS, ReactJS и Chat Engine
+Ссылка на [источник](https://www.youtube.com/watch?v=Fzv-rgwcFKk&list=PLxDiiWk5sV7-0a9fMWQLxj2S6uDm7qAQP&index=142&t=20s)
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin: true}));
+app.use(cors({ origin: true }));
 
-app.post('/authenticate', async (req, res) => {
-  const {username} = req.body;
-  return res.json({username: username, secret: 'sha256'});
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const response = await axios.put(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: username,
+        first_name: username,
+      },
+      { headers: { "private-key": process.env.PRIVATE_KEY } }
+    );
+    return res.status(response.status).json(response.data);
+  } catch (e) {
+    console.log(e);
+    return res.status(e.response.status).json(e.response.data);
+  }
 });
 
-app.listen('3000', () => {
-  console.log('Backend is running on post 3000');
+app.listen("3000", () => {
+  console.log("Backend is running on post 3000");
 });
